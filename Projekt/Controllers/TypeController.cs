9afa1abd8f0cc27e40ef4c85f0eef5807ce4fd76
Mikolaj Muszynski <195025@edu.p.lodz.pl>
2017-11-30 23:12:48 +0100

    working type view

---
+++ Projekt/Controllers/TypeController.cs
@@ -0,0 +1,134 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DotNetAppSqlDb.Models;
+using System.Diagnostics;
+using Projekt.Models;
+
+namespace DotNetAppSqlDb.Controllers
+{
+    public class TypeController : Controller
+    {
+        private MyDatabaseContext db;
+
+        public TypeController()
+        {
+            db = new MyDatabaseContext();
+        }
+
+        public ActionResult Index()
+        {
+            Trace.WriteLine("GET /Type/Index");
+
+            var types = db.Types.Select(s=>s);
+
+            return View(types);
+        }
+
+        // GET: Todos/Create
+        public ActionResult Create()
+        {
+            Trace.WriteLine("GET /Type/CreateBook");
+            return View(new Projekt.Models.Type());
+        }
+
+
+        // POST: Todos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TypeId, TypeName")] Projekt.Models.Type type)
+        {
+            Trace.WriteLine("POST /Type/Create");
+            if (ModelState.IsValid)
+            {
+                db.Types.Add(type);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(type);
+        }
+
+
+        // GET: Todos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            Trace.WriteLine("GET /Type/Edit/" + id);
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Projekt.Models.Type type = db.Types.Find(id);
+    
+            if (type == null)
+            {
+                return HttpNotFound();
+            }
+            return View(type);
+        }
+
+        // POST: Todos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TypeId, TypeName")] Projekt.Models.Type type)
+        {
+            Trace.WriteLine("POST /Type/Edit/" + type.TypeId);
+            if (ModelState.IsValid)
+            {
+                db.Entry(type).State = EntityState.Modified;
+                db.SaveChanges();
+ 
+                return RedirectToAction("Index");
+            }
+            return View(type);
+        }
+
+        // GET: Todos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            Trace.WriteLine("GET /Type/Delete/" + id);
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Projekt.Models.Type type = db.Types.Find(id);
+            if (type == null)
+            {
+                return HttpNotFound();
+            }
+            return View(type);
+        }
+
+        // POST: Todos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Trace.WriteLine("POST /Type/Delete/" + id);
+            Projekt.Models.Type type = db.Types.Find(id);
+            db.Types.Remove(type);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
